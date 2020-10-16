46b6ceaf92a41a43b7bd3f7fd1e88a4bac60b0ac
Michele Bisogno <michele.bisogno.ct@renesas.com>
2020-10-16 10:33:04 +0200

    Added check on certificate file existance

--- VPN.ps1
+++ VPN.ps1
@@ -10,10 +10,15 @@ catch
 Set-VpnConnectionIPsecConfiguration -ConnectionName "VPN-IKEv2" -AuthenticationTransformConstants SHA196 -CipherTransformConstants AES256 -EncryptionMethod AES256 -IntegrityCheckMethod SHA256 -PfsGroup None -DHGroup Group14 -PassThru -Force
 try
 {
-	Import-Certificate -FilePath ".\certificate.crt" -CertStoreLocation cert:\LocalMachine\Root
+	$certificate = ".\certificate.crt"
+	Import-Certificate -FilePath $certificate -CertStoreLocation cert:\LocalMachine\Root
 	Write-Output "Certificate imported successfully"
 }
 catch [UnauthorizedAccessException]
 {
 	Write-Output "Error: cannot add the certificate, run the script with admin rights"
+}
+catch [System.IO.FileNotFoundException]
+{
+	Write-Output "Error: $certificate does not exists"
 }
