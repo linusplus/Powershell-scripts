b9755c8af6bebbc416eb9ca9f298a84b79fd079e
linusplus <linusplus@yahoo.it>
2020-11-02 20:58:44 +0100

    Update VPN.ps1
    Variables grouped at the beginning

--- VPN.ps1
+++ VPN.ps1
@@ -1,25 +1,29 @@
 # Powershell script to automatically add a VPN connection and its certificate
-# Test
+
+$connectionName = "VPN-IKEv2"
+$server = "linusplus.duckdns.org"
+$certificate = .\certificate.crt"
+
+# Do not modify below this line
 
 try
 {
-	Add-VpnConnection -Name "VPN-IKEv2" -ServerAddress linusplus.duckdns.org -TunnelType "Ikev2"
+	Add-VpnConnection -Name $connectionName -ServerAddress $server -TunnelType "Ikev2"
 	Write-Output "VPN connection added successfully"
 }
 catch
 {
-	Write-Output "Warning: VPN connection VPN-IKEv2 already exists, attributes will be overwritten"
+	Write-Output "Warning: VPN connection $connectionName already exists, attributes will be overwritten"
 }
-Set-VpnConnectionIPsecConfiguration -ConnectionName "VPN-IKEv2" -AuthenticationTransformConstants SHA196 -CipherTransformConstants AES256 -EncryptionMethod AES256 -IntegrityCheckMethod SHA256 -PfsGroup None -DHGroup Group14 -PassThru -Force
+Set-VpnConnectionIPsecConfiguration -ConnectionName $connectionName -AuthenticationTransformConstants SHA196 -CipherTransformConstants AES256 -EncryptionMethod AES256 -IntegrityCheckMethod SHA256 -PfsGroup None -DHGroup Group14 -PassThru -Force
 try
 {
-	$certificate = ".\certificate.crt"
 	Import-Certificate -FilePath $certificate -CertStoreLocation cert:\LocalMachine\Root
 	Write-Output "Certificate imported successfully"
 }
 catch [UnauthorizedAccessException]
 {
-	Write-Output "Error: cannot add the certificate, run the script with admin rights"
+	Write-Output "Error: cannot add certificate, run the script with admin rights"
 }
 catch [System.IO.FileNotFoundException]
 {
